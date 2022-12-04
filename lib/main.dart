75ffefcfb63d96a9afe85a804368e66dbdbe289f
kurnianugroho <kurnia.nugroho@gmail.com>
2022-12-04 14:25:04 +0700

    First commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -7,9 +8,40 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              generateButton(color: Colors.red, audioFileName: 'note1.wav'),
+              generateButton(color: Colors.orange, audioFileName: 'note2.wav'),
+              generateButton(color: Colors.yellow, audioFileName: 'note3.wav'),
+              generateButton(color: Colors.green, audioFileName: 'note4.wav'),
+              generateButton(color: Colors.teal, audioFileName: 'note5.wav'),
+              generateButton(color: Colors.blue, audioFileName: 'note6.wav'),
+              generateButton(color: Colors.purple, audioFileName: 'note7.wav'),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+
+  void playSound(String fileName) {
+    final player = AudioCache();
+    player.play(fileName);
+  }
+
+  Widget generateButton({MaterialColor color, String audioFileName}) {
+    return Expanded(
+      child: TextButton(
+        child: Container(),
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(color),
+        ),
+        onPressed: () {
+          playSound(audioFileName);
+        },
       ),
     );
   }
